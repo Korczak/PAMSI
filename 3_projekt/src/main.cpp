15df0ccd912a3aed2946e25d6ac4ca5862702dda
Korczak <Szymonkor8@gmail.com>
2019-05-23 13:50:19 +0200

    created main

---
+++ 3_projekt/src/main.cpp
@@ -0,0 +1,15 @@
+#include "../inc/board.hh"
+#include<iostream>
+#include<cstdio>
+#include<cmath>
+#include<cstdlib>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	Board board();
+	board.PrintBoard();
+	system("Pause");
+	return 0;
+}
